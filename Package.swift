52b7b71ef364c3b4696f191411911deafe81d8d3
Mihael Isaev <isaev.mihael@gmail.com>
2018-07-27 03:05:31 +0400

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,17 +8,15 @@ let package = Package(
     products: [
         // Swift lib that gives ability to build complex raw SQL-queries in a more easy way using KeyPaths
         .library(name: "FluentQuery", targets: ["FluentQuery"]),
-        .library(name: "FluentQueryPostgreSQL", targets: ["FluentQuery", "FluentQueryPostgreSQL"]),
-        .library(name: "FluentQueryMySQL", targets: ["FluentQuery", "FluentQueryMySQL"]),
-    ],
+        ],
     dependencies: [
         // Swift ORM framework (queries, models, and relations) for building NoSQL and SQL database integrations.
-        .package(url: "https://github.com/vapor/fluent-postgresql.git", from: "1.0.0-rc.3.0.2"),
-    ],
+        .package(url: "https://github.com/vapor/postgresql.git", from: "1.0.0"),
+        // Event-driven network application framework for high performance protocol servers & clients, non-blocking.
+        .package(url: "https://github.com/apple/swift-nio.git", from: "1.0.0"),
+        ],
     targets: [
-        .target(name: "FluentQuery", dependencies: ["Fluent"]),
-        .target(name: "FluentQueryPostgreSQL", dependencies: ["FluentPostgreSQL", "FluentQuery"]),
-        .target(name: "FluentQueryMySQL", dependencies: ["FluentMySQL", "FluentQuery"]),
-        .testTarget(name: "FluentQueryTests", dependencies: ["FluentQuery", "FluentPostgreSQL", "FluentMySQL"]),
-    ]
+        .target(name: "FluentQuery", dependencies: ["PostgreSQL"]),
+        .testTarget(name: "FluentQueryTests", dependencies: ["FluentQuery", "PostgreSQL"]),
+        ]
 )
